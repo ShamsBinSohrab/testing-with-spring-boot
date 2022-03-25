4d1bcbd7f94112fa61af5fd4dc538e9e2be826bd
shams <aniknishan@gmail.com>
2022-03-25 22:07:57 +0600

    Initial commit

--- src/main/java/com/shams/spring/testing/TestingWithSpringBootApplication.java
+++ src/main/java/com/shams/spring/testing/TestingWithSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.shams.spring.testing.testingwithspringboot;
+package com.shams.spring.testing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/shams/spring/testing/TestingWithSpringBootApplicationTests.java
+++ src/test/java/com/shams/spring/testing/TestingWithSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shams.spring.testing.testingwithspringboot;
+package com.shams.spring.testing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
